288dd7133cbcd0cde15400f24dc9d3ca877c3431
Christopher Homer <christopher.homer@mismarine.com>
2022-06-24 13:34:33 +0100

    Lesson 11

--- src/SuggestionApp.Library/Models/UserModel.cs
+++ src/SuggestionApp.Library/Models/UserModel.cs
@@ -10,6 +10,6 @@ public class UserModel
     public string LastName { get; set; }
     public string DisplayName { get; set; }
     public string EmailAddress { get; set; }
-    public List<BasicSuggestionModel> SuggestionModels { get; set; } = new();
+    public List<BasicSuggestionModel> AuthoredSuggestions { get; set; } = new();
     public List<BasicSuggestionModel> VotedOnSuggestions { get; set; } = new();
 }
