8ce0fbfba740e7d3c3e06bc0a062174f5dd0e884
Christopher Homer <christopher.homer@mismarine.com>
2022-06-24 12:48:22 +0100

    Lesson 10

---
+++ src/SuggestionApp.Library/DataAccess/IUserData.cs
@@ -0,0 +1,10 @@
+﻿namespace SuggestionApp.Library.DataAccess;
+
+public interface IUserData
+{
+    Task CreateUser(UserModel user);
+    Task<UserModel> GetUser(string id);
+    Task<UserModel> GetUserFromAuthentication(string objectId);
+    Task<List<UserModel>> GetUsers();
+    Task UpdateUser(UserModel user);
+}
