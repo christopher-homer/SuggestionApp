bb8df3f799cdc18788447c9fb2f8ad30ae1a69c0
Christopher Homer <christopher.homer.75@gmail.com>
2022-06-24 21:29:56 +0100

    Use Autofac

---
+++ src/SuggestionApp.Library/DI/LibraryModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using SuggestionApp.Library.DataAccess;
+
+namespace SuggestionApp.Library.DI;
+
+public class LibraryModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<DbConnection>().As<IDbConnection>().SingleInstance();
+        builder.RegisterType<MongoCategoryData>().As<ICategoryData>().SingleInstance();
+        builder.RegisterType<MongoStatusData>().As<IStatusData>().SingleInstance();
+        builder.RegisterType<MongoSuggestionData>().As<ISuggestionData>().SingleInstance();
+        builder.RegisterType<MongoUserData>().As<IUserData>().SingleInstance();
+    }
+}
--- src/SuggestionApp.UI/RegisterServices.cs
+++ src/SuggestionApp.UI/RegisterServices.cs
@@ -1,4 +1,8 @@
-﻿namespace SuggestionApp.UI;
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using SuggestionApp.Library.DI;
+
+namespace SuggestionApp.UI;
 
 public static class RegisterServices
 {
@@ -8,10 +12,17 @@ public static class RegisterServices
         builder.Services.AddServerSideBlazor();
         builder.Services.AddMemoryCache();
 
-        builder.Services.AddSingleton<IDbConnection, DbConnection>();
-        builder.Services.AddSingleton<ICategoryData, MongoCategoryData>();
-        builder.Services.AddSingleton<IStatusData, MongoStatusData>();
-        builder.Services.AddSingleton<ISuggestionData, MongoSuggestionData>();
-        builder.Services.AddSingleton<IUserData, MongoUserData>();
+        //builder.Services.AddSingleton<IDbConnection, DbConnection>();
+        //builder.Services.AddSingleton<ICategoryData, MongoCategoryData>();
+        //builder.Services.AddSingleton<IStatusData, MongoStatusData>();
+        //builder.Services.AddSingleton<ISuggestionData, MongoSuggestionData>();
+        //builder.Services.AddSingleton<IUserData, MongoUserData>();
+
+        builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+        builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+        {
+            builder.RegisterModule(new LibraryModule());
+        });
+
     }
 }
