c09aa4d80f1bd4fe33aaf37de376427a6dc90e09
Christopher Homer <christopher.homer@mismarine.com>
2022-06-27 10:21:02 +0100

    Lesson 21

--- src/SuggestionApp.UI/RegisterServices.cs
+++ src/SuggestionApp.UI/RegisterServices.cs
@@ -2,6 +2,7 @@
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 using SuggestionApp.Library.DI;
 
 namespace SuggestionApp.UI;
@@ -11,8 +12,9 @@ public static class RegisterServices
     public static void ConfigureServices(this WebApplicationBuilder builder)
     {
         builder.Services.AddRazorPages();
-        builder.Services.AddServerSideBlazor();
+        builder.Services.AddServerSideBlazor().AddMicrosoftIdentityConsentHandler(); ;
         builder.Services.AddMemoryCache();
+        builder.Services.AddControllersWithViews().AddMicrosoftIdentityUI();
 
         builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
             .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAdB2C"));
